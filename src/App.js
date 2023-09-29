a2e6840dbe9eee19671a60bdf9c3e25c8c8cf65f
Mihika Saxena <mihikasaxena13@gmail.com>
2023-09-29 19:06:07 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import dataJson from './dataTable.json';
+import { Table } from 'semantic-ui-react'; //semantic ui for table layout
+import './App.css'
 
 function App() {
+
+  const [data, setData] = useState([]);  //initialise state vairable as empty array
+  useEffect(() => { // to hook to dataJson flie
+    setData(dataJson); 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+
+      <Table celled>  
+        <Table.Header>
+          <Table.Row>
+            {data.length > 0 &&
+              Object.keys(data[0]).map((key) => ( // function  checks if there is data in json file or not .
+                <Table.HeaderCell key={key}>{key}</Table.HeaderCell>
+              ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.map((item, index) => (
+            <Table.Row key={index}>
+              {Object.values(item).map((value, subIndex) => (
+                <Table.Cell key={subIndex}>{value}</Table.Cell>  // renders the value from json file
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </div>
   );
 }
